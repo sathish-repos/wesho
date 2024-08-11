fa879f7e235ab43d89f5429be1a5f859e6b073fd
sathish <me.sathish.git@gmail.com>
2024-08-11 09:14:27 +0530

    add CRUD operations for goals

--- apps/wesho/backend/src/routes/goals.routes.ts
+++ apps/wesho/backend/src/routes/goals.routes.ts
@@ -5,3 +5,15 @@ export const goalsRouter = express.Router();
 goalsRouter.get('/', (req, res) => {
   res.status(200).json({ message: 'Get goals' });
 });
+
+goalsRouter.post('/', (req, res) => {
+  res.status(200).json({ message: 'set goals' });
+});
+
+goalsRouter.put('/:id', (req, res) => {
+  res.status(200).json({ message: `update goal ${req.params.id}` });
+});
+
+goalsRouter.delete('/:id', (req, res) => {
+  res.status(200).json({ message: `delete goal ${req.params.id}` });
+});
