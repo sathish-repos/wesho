d03fdcf79e878375bbd4b7eed3cbb5edfcbc2f9a
sathish <me.sathish.git@gmail.com>
2024-08-11 10:47:43 +0530

    add async to handle mongoose promise

--- apps/wesho/backend/src/controller/goals.controller.ts
+++ apps/wesho/backend/src/controller/goals.controller.ts
@@ -1,14 +1,14 @@
 // @desc Get goals
 // @route GET api/goals
 // @access Private
-export const getGoals = (req, res) => {
+export const getGoals = async (req, res) => {
   res.status(200).json({ message: 'Get goals' });
 };
 
 // @desc Set goals
 // @route POST api/goals
 // @access Private
-export const setGoals = (req, res) => {
+export const setGoals = async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error('Please add a text field');
@@ -19,13 +19,13 @@ export const setGoals = (req, res) => {
 // @desc update goal
 // @route PUT api/goals/:id
 // @access Private
-export const updateGoal = (req, res) => {
+export const updateGoal = async (req, res) => {
   res.status(200).json({ message: `update goal ${req.params.id}` });
 };
 
 // @desc delete goal
 // @route DELETE api/goals/:id
 // @access Private
-export const deleteGoal = (req, res) => {
+export const deleteGoal = async (req, res) => {
   res.status(200).json({ message: `delete goal ${req.params.id}` });
 };
