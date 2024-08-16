0a5372eb02a5fc979c85672152592beab9fbce09
sathish <me.sathish.git@gmail.com>
2024-08-16 15:10:09 +0530

    add routing

--- apps/wesho/frontend/src/app/app.component.ts
+++ apps/wesho/frontend/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'we-root',
+  imports: [RouterOutlet],
+  selector: 'wesho-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- apps/wesho/frontend/src/app/app.routes.ts
+++ apps/wesho/frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { CartComponent } from './pages/cart/cart.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: 'cart', component: CartComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+];
