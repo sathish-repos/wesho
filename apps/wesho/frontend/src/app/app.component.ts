4d663953ff8fcc6d1ade9739853b2c32c494c2bc
sathish <me.sathish.git@gmail.com>
2024-08-16 17:04:27 +0530

    add nav bar

--- apps/wesho/frontend/src/app/app.component.ts
+++ apps/wesho/frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from '@wesho/shared-ui';
 
 @Component({
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent],
   selector: 'wesho-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
