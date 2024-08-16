26c7fcee400e9930426f08b77ccc4f5b50a59ced
sathish <me.sathish.git@gmail.com>
2024-08-16 09:28:11 +0530

    add product routes

--- apps/wesho/backend/src/routes/api.routes.ts
+++ apps/wesho/backend/src/routes/api.routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { goalsRouter } from './goals.routes';
 import { usersRouter } from './users.routes';
+import { productsRouter } from './products.routes';
 
 export const apiRouter = express.Router();
 
@@ -9,5 +10,7 @@ apiRouter.get('/', (req, res) => {
   res.send({ message: 'Welcome to wesho-backend api page!' });
 });
 
+// it's /api/goals route handler
 apiRouter.use('/goals', goalsRouter);
 apiRouter.use('/users', usersRouter);
+apiRouter.use('/products', productsRouter);
